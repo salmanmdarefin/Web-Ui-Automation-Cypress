0260945cedf246bdedd572e5cf4ab1315d0da074
Md.Salman Arefin <73155268+salmanmdarefin@users.noreply.github.com>
2023-07-29 16:50:10 +0600

    Add files via upload

---
+++ Cypress/cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+//Md.Salman Arefin.
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
